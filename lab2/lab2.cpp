2c9208e008a021207ed5baacdef5666546ff7bd6
Tut-Turuu <151996563+Tut-Turuu@users.noreply.github.com>
2025-06-23 13:48:43 +0200

    Add files via upload

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -12,41 +12,47 @@ int char_to_index(const char c) {
 
 class CharTable {
     char*** alphabet;
+    int size;
 public:
 
-    CharTable(const std::string filename) {
+    CharTable(const std::string filename, int size): size(size) {
         alphabet = new char**[27];
 
         for (int i = 0; i < 27; ++i) {
-            alphabet[i] = new char*[7];
-            for (int j = 0; j < 7; ++j) {
-                alphabet[i][j] = new char[7];
+            alphabet[i] = new char*[size];
+            for (int j = 0; j < size; ++j) {
+                alphabet[i][j] = new char[size];
             }
         }
-
+        
         std::ifstream fin;
         fin.open(filename);
 
 
         char trash;
         
-        for (int i = 0; i < 7; ++i) {
+        for (int i = 0; i < size; ++i) {
             for (int j = 0; j < 27; ++j) {
                 
-                fin.read(alphabet[j][i], 7);
+                fin.read(alphabet[j][i], size);
                 
             }
             fin.read(&trash, 1);
         }
         fin.close();
+        
     }
     int get_row_count() {
-        return 7;
+        
+        return size;
     }
+
     std::string get_row(char c, int n) {
         std::string str;
-        str.resize(7);
-        memcpy(str.data() , alphabet[c][n], 7);
+        str.resize(size);
+        
+        memcpy(str.data() , alphabet[c][n], size);
+        
         return str;
     }
 };
@@ -62,26 +68,43 @@ class Printer {
     Color color;
     std::pair<int, int> position;
     char symbol;
-
-    static CharTable char_table;
-
+    int size;
 public:
+    static CharTable char_table5;
+    static CharTable char_table7;
 
-    Printer(Color color, std::pair<int, int> position, char symbol): color(color), position(position), symbol(symbol) {}
+
+
+
+    Printer(Color color, std::pair<int, int> position, char symbol, int char_size): color(color), position(position), symbol(symbol), size(char_size) {}
 
     void print(std::string text) {
+        CharTable* char_table;
+
+        if (size == 5) {
+            char_table = &Printer::char_table5;
+        } else if (size == 7) {
+            char_table = &Printer::char_table7;
+        } else {
+            std::cerr << "cant find font with size: " << size << std::endl;
+            return ;
+        }
+
         for (int i = 0; i < position.second; ++i) {
             std::cout << '\n';
         }
 
         std::string tmp;
         std::cout << "\u001b[" << (int)color << "m";
-        for (int i = 0; i < char_table.get_row_count(); ++i) {
+        
+        for (int i = 0; i < char_table->get_row_count(); ++i) {
+            
             for (int j = 0; j < position.first; ++j) {
                 std::cout << ' ';
             }
+            
             for (char c: text) {
-                tmp = char_table.get_row(char_to_index(c), i);
+                tmp = char_table->get_row(char_to_index(c), i);
                 for (char ch: tmp) {
                     if (ch == '#') {
                         std::cout << symbol;
@@ -97,8 +120,8 @@ public:
         std::cout << "\u001b[0m";
     }
 
-    static void print(std::string text, Color color, std::pair<int, int> position, char symbol) {
-        Printer printer(color, position, symbol);
+    static void print(std::string text, Color color, std::pair<int, int> position, char symbol, int char_size) {
+        Printer printer(color, position, symbol, char_size);
         printer.print(text);
     
     }
@@ -107,17 +130,19 @@ public:
 
 };
 
+CharTable Printer::char_table5("ascii_5*5.txt", 5);
 
-CharTable Printer::char_table("ascii_7*7.txt");
+CharTable Printer::char_table7("ascii_7*7.txt", 7);
 
 int main() {
 
-    Printer printer(Color::Blue, std::pair<int, int>(1, 1), '?');
+
+    Printer printer(Color::Blue, std::pair<int, int>(1, 1), '?', 5);
 
     printer.print("hello oop");
 
-    Printer::print("abc", Color::Red, std::pair<int, int>(10,2), '1');
-    Printer::print("red", Color::Green, std::pair<int, int>(0,0), '@');
+    Printer::print("abc", Color::Red, std::pair<int, int>(10,2), '1', 7);
+    Printer::print("red", Color::Green, std::pair<int, int>(0,0), '@', 5);
 
 
 }
